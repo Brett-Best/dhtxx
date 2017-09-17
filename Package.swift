d8280e78c5261e87ab26bc309c69d73bf036668f
Luis de la Rosa <luis@luisdelarosa.com>
2017-09-17 15:53:11 -0400

    Change to be more Swift Package Manager compliant

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "dhtxx",
+    dependencies: [
+        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0)    ]
+)
