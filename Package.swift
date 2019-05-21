fe0cf25443939cf83b1fca31db97299d24927df6
Brett Best <brettbest@live.com.au>
2019-05-21 23:05:08 +1000

    Updated package file

--- Package.swift
+++ Package.swift
@@ -4,7 +4,13 @@ import PackageDescription
 
 let package = Package(
   name: "dhtxx",
+  products: [
+    .library(name: "dhtxx", targets: ["dhtxx"])
+  ],
   dependencies: [
     .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "2.0.0-beta1")
+  ],
+  targets: [
+    .target(name: "dhtxx", dependencies: ["SwiftyGPIO"])
   ]
 )
